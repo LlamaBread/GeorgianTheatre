65c7ef078dddc85df2cd9e59dfb61309a0e03c23
Llama-bread <49031977+LlamaBread@users.noreply.github.com>
2019-04-09 12:02:51 +0100

    Update selection.vue

--- src/components/selection.vue
+++ src/components/selection.vue
@@ -1,28 +1,57 @@
 <template>
   <div id = "parent">
-
-          <div id = "wrapper">
-            <div class = "speech-wrapper">
-            <div class = "speech-bubble">
-              <h3>Tell us what you love...</h3>
+    <div id = "wrapper">
+      <div class = "speech-wrapper">
+        <div class = "speech-bubble">
+          <h3>Refreshments</h3>
+        </div>
+      </div>
+      <div class="main-text">
+        <p>Welcome to The Georgian Theatre, Stockton-on-Tees – Teesside’s longest running and one of north-east England’s finest independent music venues…<br><br>
+          We provide a great programme of live music – and when we throw our doors open, anyone can pop by for a drink and a chat in our bar area - serving fine real ales, craft beers, gins and ciders.<br><br>
+          We’re child and dog friendly  – and we think The Georgian Theatre is a really great place just to gather and hang out…<br><br>
+          Although we don't currently open regularly during the day for food and drink; we offer a catering service for special events and bookings.</p>
+      </div>
+      <div id="bar">
+        <div class="g-container">
+            <div id="selections-container">
+              <div class="selection-item">
+                <div class = "speecher-wrapper">
+                  <div class = "speecher-bubble">
+                    <h3>Bar Opening</h3>
+                  </div>
+                </div>
+              <p>WE OPEN OUR BAR FROM 5:00PM ON THE NIGHTS WE HAVE EVENTS RUNNING.<br>NON-TICKET HOLDERS ARE WELCOME TO USE OUR BAR.</p><br>
+              <p>SERVING...</p>
+              <p>REAL ALES</p>
+              <p>PREMIUM GINS</p>
+              <p>FRESH COFFEES</p>
             </div>
-            </div>
-
-            <div id = "selections-container">
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/punk.jpg">Punk</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/rock.jpg">Rock</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/metal.jpg">Metal</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/rnb.jpg">RNB</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/pop.jpg">Pop</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/rap.jpg">Rap</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/dance.jpg">Dance</div>
-              <div class = "selection-item" v-on:click="selectedItem"><img src="../assets/retro.jpg">Retro</div>
-            </div>
+          <div class="selection-item">
+            <img src="../assets/gtbar.jpg">
           </div>
-
-          <div class = "button-wrap">
-            <router-link to="/show-gigs"><button id = "get-listings" v-on:click="getValue"> {{name}} </button></router-link>
+        </div>
+        </div>
+      </div>
+      <div class="main-text">
+        <div class = "speechs-wrapper">
+          <div class = "speechs-bubble">
+            <h3>Hot Drinks</h3>
           </div>
+        </div>
+        <ul>
+          <li>Americano - £1.60</li>
+          <li>CAPPUCCINO - £2.00</li>
+          <li>FLAT WHITE - £2.50 </li>
+          <li>LATTE - £2.00</li>
+          <li>MOCHA -  £2.60</li>
+          <li>HOT CHOCOLATE - £2.20</li>
+          <li>POT OF TEA FOR ONE - £1.70</li>
+          <li>POT OF TEA FOR TWO - £3.00</li>
+        </ul>
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,36 +59,33 @@
 export default {
   data () {
     return {
-      name: "Show Upcoming Gigs",
-      selectedItems: []
     }
   },
   methods:{
-    getValue() {
-      console.log(this.selectedItems);
-      //Put items into session store here.
-      sessionStorage.setItem("music_Selection", JSON.stringify(this.selectedItems));
 
-    },
-    selectedItem(event){
-
-        let item = event.target.textContent;
-        let arr = this.selectedItems;
-
-        if (arr.includes(item)){
-            return
-        }
-
-      this.selectedItems.push(event.target.textContent);
-      event.target.style.backgroundColor = "#3A3C39";
-      event.target.style.color = "white";
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.g-container {
+  margin: 10px;
+}
+#selections-container {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  text-align: center;
+  background-color: #6C7C59;
+  color: white;
+  padding: 10px;
+}
+#selections-container p{
+  margin-top: 5px;
+}
 #parent {
   width: 100%;
   text-align: center;
@@ -72,6 +98,15 @@ export default {
 }
 #wrapper {
   position: relative;
+}
+.main-text {
+  background: #3A3C39;
+  color: white;
+  margin: 10px;
+  padding: 20px;
+}
+p {
+  margin: 0px;
 }
 .speech-wrapper {
   margin-bottom: 12px;
@@ -93,47 +128,38 @@ h3 {
   font-weight: lighter;
   margin-top: 12px;
 }
-#selections-container {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  text-align: center;
-}
-#selections-container div {
-  min-width: 140px;
-  min-height: 130px;
-  text-align: center;
-}
-#get-listings{
-  text-align: center;
-  width: 240px;
-  height: 40px;
-  margin: 0 auto;
-  position: relative;
-  background-color: #6C7C59;
-  color: white;
-  border: none;
-  font-size: 18px;
-}
-.selection-item {
-  background-color: #F8F8F8;
-  -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 88%, 50% 75%, 0 75%);
-  clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 88%, 50% 75%, 0 75%);
-}
 img {
-  width: 120px;
-  height: auto;
-  display: block;
-  margin-left: 10px;
+  max-width: 100%;
   margin-top: 5px;
 }
-.button-wrap {
- text-align: center;
-    margin: 15px;
+.speechs-wrapper {
+  margin-bottom: 12px;
 }
-.i-am-active {
-  background-color: #3A3C39 !important;
+.speechs-bubble h3 {
+  color: #6C7C59;
+}
+.speechs-bubble {
+  width: 300px;
+  height: 65px;
+  font-size: 18px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  background-color: white;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.speecher-bubble {
+  width: 300px;
+  height: 65px;
+  font-size: 18px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  background-color: #3A3C39;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
