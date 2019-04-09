5f350c42e6081402dffd6b80c83998f048a64da8
Llama-bread <49031977+LlamaBread@users.noreply.github.com>
2019-04-09 15:26:51 +0100

    Update refreshments.vue

--- src/components/refreshments.vue
+++ src/components/refreshments.vue
@@ -6,69 +6,94 @@
           <h3>Refreshments</h3>
         </div>
       </div>
-      <div id="main-text">
+      <div class="main-text">
         <p>Welcome to The Georgian Theatre, Stockton-on-Tees – Teesside’s longest running and one of north-east England’s finest independent music venues…<br><br>
           We provide a great programme of live music – and when we throw our doors open, anyone can pop by for a drink and a chat in our bar area - serving fine real ales, craft beers, gins and ciders.<br><br>
           We’re child and dog friendly  – and we think The Georgian Theatre is a really great place just to gather and hang out…<br><br>
           Although we don't currently open regularly during the day for food and drink; we offer a catering service for special events and bookings.</p>
       </div>
-      <div id="bar">
         <div class="g-container">
-            <div id="container">
-              <div class="g-item">
-              <h3>Bar Opening</h3>
+            <div id="selections-container">
+              <div class="selection-item">
+                <div class = "speecher-wrapper">
+                  <div class = "speecher-bubble">
+                    <h3>Bar Opening</h3>
+                  </div>
+                </div>
               <p>WE OPEN OUR BAR FROM 5:00PM ON THE NIGHTS WE HAVE EVENTS RUNNING.<br>NON-TICKET HOLDERS ARE WELCOME TO USE OUR BAR.</p><br>
               <p>SERVING...</p>
               <p>REAL ALES</p>
               <p>PREMIUM GINS</p>
               <p>FRESH COFFEES</p>
+              <div id="slider">
+                <app-slider></app-slider>
             </div>
-          <div class="g-item">
-            <img src="../assets/gtbar.jpg">
+            </div>
+        </div>
+        </div>
+
+      <div class="main-text">
+        <div class = "speechs-wrapper">
+          <div class = "speechs-bubble">
+            <h3>Hot Drinks</h3>
           </div>
         </div>
-        </div>
+        <img id="coffee" src="../assets/coffee.svg">
+          <li>Americano - £1.60</li>
+          <li>CAPPUCCINO - £2.00</li>
+          <li>FLAT WHITE - £2.50 </li>
+          <li>LATTE - £2.00</li>
+          <li>MOCHA -  £2.60</li>
+          <li>HOT CHOCOLATE - £2.20</li>
+          <li>POT OF TEA FOR ONE - £1.70</li>
+          <li>POT OF TEA FOR TWO - £3.00</li>
       </div>
       </div>
-    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-    }
-  },
-  methods:{
+import Slider from './Slider.vue';
 
-    }
+export default {
+  components: {
+    'app-slider':Slider
+  },
+  data() {
+    return{}
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .g-container {
+  margin: 10px;
+}
+#slider {
+  margin: 10px;
+}
+#coffee {
+  margin-left: 30px;
+}
+li {
+  list-style: none;
+}
+#selections-container {
   width: 100%;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-evenly;
   text-align: center;
-}
-.g-container div {
-  min-width: 140px;
-  min-height: 130px;
-  text-align: center;
-}
-.g-item {
-  color: white;
-}
-#container {
-  width: 100%;
-  padding: 10px;
-  margin: 10px;
   background-color: #6C7C59;
+  color: white;
+  padding: 10px;
+  padding-bottom: 25px;
+}
+#selections-container p{
+  margin-top: 5px;
 }
 #parent {
   width: 100%;
@@ -83,11 +108,12 @@ export default {
 #wrapper {
   position: relative;
 }
-#main-text {
+.main-text {
   background: #3A3C39;
   color: white;
   margin: 10px;
   padding: 20px;
+  text-align: center;
 }
 p {
   margin: 0px;
@@ -112,8 +138,38 @@ h3 {
   font-weight: lighter;
   margin-top: 12px;
 }
+.speechs-wrapper {
+  margin-bottom: 12px;
+}
+.speechs-bubble h3 {
+  color: #6C7C59;
+}
+.speechs-bubble {
+  width: 300px;
+  height: 65px;
+  font-size: 18px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  background-color: white;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.speecher-bubble {
+  width: 300px;
+  height: 65px;
+  font-size: 18px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 65% 75%, 66% 96%, 50% 75%, 0 75%);
+  background-color: #3A3C39;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
 img {
-  max-width: 50%;
-  margin-top: 5px;
+  max-width: 100%;
+  margin-top: 10px;
 }
 </style>
