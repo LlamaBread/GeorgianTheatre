29df8a61a21d93754002e21753734a39296fb1ed
Llama-bread <49031977+LlamaBread@users.noreply.github.com>
2019-04-11 15:53:00 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import SelectScreen from './components/selection.vue';
 import Launchbtn from './components/launch-button.vue';
 import GigList from './components/show-gigs.vue';
 import Refreshments from './components/refreshments.vue';
+import Gallery from './components/gallery.vue'
 
 export default [
     {path: '/', component: Launchbtn },
     {path: '/selection', component: SelectScreen },
     {path: '/show-gigs', component: GigList },
-    {path: '/refreshments', component: Refreshments}
+    {path: '/refreshments', component: Refreshments},
+    {path: '/gallery', component: Gallery}
 ]
